8508e6e222a2f35d3c7f19fae3f3785c1d4a1d63
MauroVA <maurova98@gmail.com>
2020-10-19 14:52:27 +0200

    OOP

--- bay_assignment.py
+++ bay_assignment.py
@@ -1,131 +1,205 @@
 """
 The Bay Assignment Problem
-Authors: Mauro, Luke    2020
+Authors: Mauro, Luke   2020
 """
 # Importing modules
-from collections import defaultdict
+from collections import defaultdict, ChainMap
 from pulp import LpProblem, LpMinimize, lpSum, LpInteger, LpVariable, LpStatus, value
 from math import ceil
-import datetime as dt
+from datetime import datetime, timedelta, time
 
-from flight_schedule import schedule, ac_dict
-
-
-def pref(index, val):
-    costs[index[0]][index[1]] = costs[index[0]][index[1]]*(1/val)
+from flight_schedule import Scheduler
 
 
-def get_dt(t: dt.time):
-    return dt.datetime(1, 1, 1, t.hour, t.minute)
-
-
-def alp_list(start: str, end: str):
+def cat_list(start: str, end: str):
     w = [chr(i) for i in range(ord(start), ord(end) + 1)]
     return w[::-1]
 
 
-# variables
-# start time, end time, buffer times
-t_start = dt.time(hour=8, minute=0)
-t_end = dt.time(hour=22, minute=0)
+class LPSolver(object):
+    def __init__(self, date: datetime = datetime(1, 1, 1), arr_buf: timedelta = timedelta(minutes=15),
+                 dep_buf: timedelta = timedelta(minutes=15)):
 
-tarr_buf = dt.timedelta(minutes=15)
-tdep_buf = dt.timedelta(minutes=15)
+        self.__schedule = Scheduler(date)
+        self.__turns = self.__schedule.return_turns()
+        self.__lturns = self.__schedule.return_lturns()
+        self.__ac = self.__schedule.return_ac()
 
-# terminals and amount of gates
-terminals = ["A", "B"]  # A: domestic + continental terminal / B: international terminal
-n_bays = [15, 10]
+        self.__arr_buf = arr_buf
+        self.__dep_buf = dep_buf
 
-bays = {}
-costs = defaultdict(dict)
-t_penalty = 20
+        # bay variables
+        self.__terminals = ["A", "B"]  # A: domestic + continental terminal / B: international terminal
+        self.__nbays = [15, 10]
+        self.__ter_penalty = 100  # penalty multiplier for inappropriate terminal
 
-# Create a dictionary with bays
-for idx, ter in enumerate(terminals):
-    for b in range(1, n_bays[idx] + 1):
-        if ter == "B":
-            if b <= 2:
-                bays[ter + str(b)] = {"dist": ceil(b / 2) * 4 - 2, "cat": alp_list("B", "H"), "type": "INT"}
-            elif 2 < b <= 8:
-                bays[ter + str(b)] = {"dist": ceil(b / 2) * 3 - 0.5, "cat": alp_list("B", "G"), "type": "INT"}
-            else:
-                bays[ter + str(b)] = {"dist": 20, "cat": alp_list("A", "G"), "type": "INT"}
-        elif ter == "A":
-            if b <= 2:
-                bays[ter + str(b)] = {"dist": ceil(b / 2) * 3 - 1.5, "cat": alp_list("B", "H"), "type": "DOM"}
-            elif 2 < b <= 10:
-                bays[ter + str(b)] = {"dist": ceil(b / 2) * 2, "cat": alp_list("B", "E"), "type": "DOM"}
-            else:
-                bays[ter + str(b)] = {"dist": 20, "cat": alp_list("A", "G"), "type": "DOM"}
+        self.__int_lbays = 2
+        self.__int_sbays = 6
+        self.__dom_lbays = 2
+        self.__dom_sbays = 8
 
-# create cost matrix
-for i in schedule:
-    for k in bays:
-        if schedule[i]["type"] == bays[k]["type"]:
-            costs[i][k] = ac_dict[schedule[i]["ac"]]["cap"] * bays[k]["dist"]
+        # create bay data dictionary
+        self.__bays = self.get_bays(terminals=self.__terminals, nbays=self.__nbays)
+
+        # create chain map of all turns
+        self.__map_turns = ChainMap(self.__turns, self.__lturns["FULL"], self.__lturns["SPLIT"])
+
+        # get costs matrices for turns and tows for objective function
+        self.__costs_turns = self.get_costs_turns()
+        self.__costs_tows = self.get_costs_tows()
+
+        self.__tconf = self.get_tconf()
+
+        # Creates the 'prob' variable to contain the problem data
+        self.__prob = LpProblem("Bay_Assignment", LpMinimize)
+
+        # Creates a list of tuples containing all the possible combinations of flight and bays
+        self.__keys = [(i, k) for i in self.__map_turns for k in self.__bays]
+
+        self.__var_turns, self.__var_tows = self.make_vars()
+
+        self.make_objf()
+        self.make_cons()
+
+        self.writeLP()
+        self.solve()
+
+    def get_bays(self, terminals: list, nbays: list):
+        bays = {}
+        # Create a dictionary with bay data
+        for idx, ter in enumerate(terminals):
+            for k in range(1, nbays[idx] + 1):
+                if ter == "B":
+                    bays[ter + str(k)] = {"type": "INT"}
+                    if k <= self.__int_lbays:
+                        bays[ter + str(k)].update({"dist": ceil(k / 2) * 4 - 2, "cat": cat_list("B", "H")})
+                    elif k <= self.__int_lbays + self.__int_sbays:
+                        bays[ter + str(k)].update({"dist": ceil(k / 2) * 3 - 0.5, "cat": cat_list("B", "G")})
+                    else:
+                        bays[ter + str(k)].update(
+                            {"dist": 10 + ceil((self.__int_lbays + self.__int_sbays) / 2) * 3 - 0.5,
+                             "cat": cat_list("A", "G")})
+                elif ter == "A":
+                    bays[ter + str(k)] = {"type": "DOM"}
+                    if k <= self.__dom_lbays:
+                        bays[ter + str(k)].update({"dist": ceil(k / 2) * 3 - 1.5, "cat": cat_list("B", "H")})
+                    elif k <= self.__dom_lbays + self.__dom_sbays:
+                        bays[ter + str(k)].update({"dist": ceil(k / 2) * 2, "cat": cat_list("B", "E")})
+                    else:
+                        bays[ter + str(k)].update({"dist": 10 + ceil((self.__dom_lbays + self.__dom_sbays) / 2) * 2,
+                                                   "cat": cat_list("A", "G")})
+        return bays
+
+    def get_costs_turns(self):
+        # create cost matrix
+        costs_turns = defaultdict(dict)
+        for i in self.__map_turns:
+            a = 1 if isinstance(i, int) else 3
+            for k in self.__bays:
+                if self.__map_turns[i]["type"] == self.__bays[k]["type"]:
+                    costs_turns[i][k] = self.__ac[self.__map_turns[i]["ac"]]["cap"] * self.__bays[k]["dist"] / a
+                else:
+                    costs_turns[i][k] = self.__ter_penalty * self.__ac[self.__map_turns[i]["ac"]]["cap"] * \
+                                        self.__bays[k]["dist"] / a
+
+            if "PREF" in self.__map_turns[i]:
+                bay_pref = self.__map_turns[i]["PREF"][0]
+                pref = self.__map_turns[i]["PREF"][1]
+                costs_turns[i][bay_pref] = costs_turns[i][bay_pref] / pref
+        return costs_turns
+
+    def get_costs_tows(self):
+        costs_tows = {}
+        tow_cat = {"A": 3000, "B": 3000, "C": 4500, "D": 4500, "E": 4500, "F": 4500, "G": 9000, "H": 9000}
+        for t in self.__lturns["FULL"]:
+            costs_tows[t] = tow_cat[self.__ac[self.__lturns["FULL"][t]["ac"]]["cat"]]
+        return costs_tows
+
+    def get_tconf(self):
+        lst = []
+        for i1 in self.__map_turns:
+            arr1, dep1, n1 = self.get_buft(flight=i1)
+            for i2 in self.__map_turns:
+                arr2, dep2, n2 = self.get_buft(flight=i2)
+                if (arr2 <= arr1 <= dep2 or arr2 <= dep1 <= dep2) and n1 != n2:
+                    lst.append([i1, i2])
+        # removing reversed elements in list
+        lst = [v for k, v in enumerate(lst) if v[::-1] not in lst[:k] and v not in lst[:k]]
+        return lst
+
+    def get_buft(self, flight):
+        arr = self.__map_turns[flight]["ETA"]
+        dep = self.__map_turns[flight]["ETD"]
+        if isinstance(flight, int):
+            n = flight
+            arr -= self.__arr_buf
+            dep += self.__dep_buf
         else:
-            costs[i][k] = t_penalty*ac_dict[schedule[i]["ac"]]["cap"] * bays[k]["dist"]
+            n = int(flight[0])
+            if flight[-1] == "A":
+                arr -= self.__arr_buf
+            elif flight[-1] == "D":
+                dep += self.__dep_buf
+        return arr, dep, n
 
-# specific flight/bay preferences
-# 1 (no preference) - 10 (high preference)
-pref((3, "B2"), 10)
-pref((2, "B2"), 5)
+    def make_vars(self):
+        var_turns = LpVariable.dicts("x", ([i for i in self.__map_turns], [k for k in self.__bays]), 0, 1, LpInteger)
+        var_tows = LpVariable.dicts("w", ([i for i in self.__lturns["FULL"]]), 0, 1, LpInteger)
+        return var_turns, var_tows
 
-# Creates the 'prob' variable to contain the problem data
-prob = LpProblem("Bay_Assignment", LpMinimize)
+    def make_objf(self):
+        self.__prob += lpSum([self.__var_turns[i][k] * self.__costs_turns[i][k] for (i, k) in self.__keys] +
+                             [self.__var_tows[t] * self.__costs_tows[t] for t in self.__lturns["FULL"]]), "obj_fun"
 
-# Creates a list of tuples containing all the possible combinations of flight and bays
-keys = [(i, k) for i in schedule for k in bays]
+    def make_cons(self):
+        self.make_const_bay()
+        self.make_const_asg()
+        self.make_const_t()
 
-# A dictionary called 'Vars' is created to contain the referenced variables
-var = LpVariable.dicts("X", ([i for i in schedule], [k for k in bays]), 0, 1, LpInteger)
+    def make_const_bay(self):
+        for (i, k) in self.__keys:
+            if self.__ac[self.__map_turns[i]["ac"]]["cat"] not in self.__bays[k]["cat"] or \
+                    ((isinstance(i, str) and i[-1] == "P") and
+                     ((k[0] == "A" and int(k.replace("A", "")) <= self.__dom_lbays + self.__dom_sbays) or
+                      (k[0] == "B" and int(k.replace("B", "")) <= self.__int_lbays + self.__int_sbays))):
+                self.__prob += lpSum(self.__var_turns[i][k]) == 0, "BayConstraint_Bay:%s_Flight:%s" % (i, k)
 
-# The objective function is added to 'prob' first
-prob += lpSum([var[i][k] * costs[i][k] for (i, k) in keys]), "obj_fun"
+    def make_const_asg(self):
+        for i in self.__turns:
+            self.__prob += lpSum([self.__var_turns[i][k] for k in self.__bays]) == 1, "AssignmentConstraint_Flight:%s" % i
+        for i in self.__lturns["FULL"]:
+            self.__prob += lpSum(self.__var_tows[i] + [self.__var_turns[i][k] for k in self.__bays]) == 1, \
+                           "AssignmentConstraint_FullTow_Flight:%s" % i
+        for i in self.__lturns["SPLIT"]:
+            self.__prob += lpSum(-self.__var_tows[int("".join(x for x in i if x not in "PAD"))] +
+                                 [self.__var_turns[i][k] for k in self.__bays]) == 0, \
+                           "AssignmentConstraint_SplitTow_Flight:%s" % i
 
-t_conf = []
-for n in schedule:
-    n_arr = get_dt(schedule[n]["ETA"]) - tarr_buf
-    n_dep = get_dt(schedule[n]["ETD"]) + tdep_buf
+    def make_const_t(self):
+        for x in self.__tconf:
+            for k in self.__bays:
+                self.__prob += lpSum(self.__var_turns[x[0]][k] + self.__var_turns[x[1]][k]) <= 1, \
+                               "TimeConstraint_Flights:%s&%s_Bay:%s" % (x[0], x[1], k)
 
-    for x in range(n+1, schedule.__len__()+1):
-        x_arr = get_dt(schedule[x]["arrival"]["ETA"]) - tarr_buf
-        x_dep = get_dt(schedule[x]["departure"]["ETD"]) + tdep_buf
+    def writeLP(self):
+        # The problem data is written to an .lp file
+        self.__prob.writeLP("BayAssignmentProblem.lp")
 
-        if n_arr <= x_arr <= n_dep or n_arr <= x_dep <= n_dep:
-            t_conf.append([n, x])
+    def solve(self):
+        # The problem is solved using PuLP's choice of Solver
+        self.__prob.solve()
 
-av_bays = {}
-for f in schedule:
-    l = []
-    for b in bays:
-        if ac_dict[schedule[f]["ac"]]["cat"] not in bays[b]["cat"]:
-            prob += lpSum(var[b][f]) == 0, "Bay_Constraint:%s_%s" % (b, f)
-        else:
-            l.append(b)
-    prob += lpSum(var[b][f] for b in l) == 1, "Assignment_Constraint:%s" % f
-    av_bays[f] = l
+        # The status of the solution is printed to the screen
+        print("Status:", LpStatus[self.__prob.status])
 
-for c in t_conf:
-    for z in av_bays[c[0]]:
-        if z in av_bays[c[1]]:
-            prob += lpSum(var[z][c[0]] + var[z][c[1]]) <= 1, "TimeSlot_Constraint:%s_%s_%s" % (z, c[0], c[1])
+        # Each of the variables is printed with it's resolved optimum value
+        for v in self.__prob.variables():
+            if v.varValue == 1:
+                print(v.name, "=", v.varValue)
+
+        # The optimised objective function value is printed to the screen
+        print("Objective Function Value = ", value(self.__prob.objective))
 
 
-# Solving LP Problem
-# The problem data is written to an .lp file
-prob.writeLP("BayAssignmentProblem.lp")
-
-# The problem is solved using PuLP's choice of Solver
-prob.solve()
-
-# The status of the solution is printed to the screen
-print("Status:", LpStatus[prob.status])
-
-# Each of the variables is printed with it's resolved optimum value
-for v in prob.variables():
-    if v.varValue == 1:
-        print(v.name, "=", v.varValue)
-
-# The optimised objective function value is printed to the screen
-print("Objective Function Value = ", value(prob.objective))
+if __name__ == "__main__":
+    solver = LPSolver()
--- flight_schedule.py
+++ flight_schedule.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 
 
-class scheduler(object):
-    def __init__(self, date: datetime = datetime(2010, 4, 1)):
+class Scheduler(object):
+    def __init__(self, date: datetime = datetime(1, 1, 1)):
         self.__date = date
         self.__ttowing = timedelta(hours=4)
 
@@ -24,32 +24,50 @@ class scheduler(object):
                      "Boeing 747-800I": {"cap": 410, "cat": "H"},
                      "Airbus A380-800": {"cap": 544, "cat": "H"}}
 
-        self.__turns = {1: {"ac": "Boeing 777-300", "type": "INT", "ETA": self.get_dt(hour=10, minute=25),
-                               "ETD": self.get_dt(hour=15, minute=30)},
-                        2: {"ac": "Airbus A330-300", "type": "INT", "ETA": self.get_dt(hour=12, minute=25),
+        self.__schedule = {1: {"ac": "Boeing 777-300", "type": "INT", "ETA": self.get_dt(hour=10, minute=25),
+                               "ETD": self.get_dt(hour=15, minute=30), "PREF": [["B4", 5], ["B3", 10]]},
+                           2: {"ac": "Airbus A330-300", "type": "INT", "ETA": self.get_dt(hour=12, minute=25),
                                "ETD": self.get_dt(hour=14, minute=30)},
-                        3: {"ac": "Airbus A380-800", "type": "INT", "ETA": self.get_dt(hour=16, minute=25),
+                           3: {"ac": "Airbus A380-800", "type": "INT", "ETA": self.get_dt(hour=16, minute=25),
                                "ETD": self.get_dt(hour=17, minute=30)}}
 
-        self.__lturns = self.pross_lturns()
+        self.__turns, self.__lturns = self.pross_schedule()
 
     def get_dt(self, hour: int, minute: int):
         return datetime(self.__date.year, self.__date.month, self.__date.month, hour, minute)
 
-    def pross_lturns(self):
+    def pross_schedule(self):
+        turns = self.__schedule.copy()
         lturns = defaultdict(dict)
-        for flight in self.__turns:
-            if self.__turns[flight]["ETD"] - self.__turns[flight]["ETA"] > self.__ttowing:
-                lturns[flight]["A"] = self.__turns[flight].copy()
-                lturns[flight]["P"] = self.__turns[flight].copy()
-                lturns[flight]["D"] = self.__turns[flight].copy()
+        for flight in self.__schedule:
+            if self.__schedule[flight]["ETD"] - self.__schedule[flight]["ETA"] > self.__ttowing:
+                lturns["FULL"][flight] = self.__schedule[flight].copy()
+                lturns["SPLIT"][str(flight)+"A"] = self.__schedule[flight].copy()
+                lturns["SPLIT"][str(flight)+"P"] = self.__schedule[flight].copy()
+                lturns["SPLIT"][str(flight)+"D"] = self.__schedule[flight].copy()
 
-                lturns[flight]["A"]["ETD"] = self.__turns[flight]["ETA"] + timedelta(minutes=45)
-                lturns[flight]["D"]["ETA"] = self.__turns[flight]["ETD"] - timedelta(minutes=45)
-                lturns[flight]["P"]["ETA"] = lturns[flight]["A"]["ETD"]
-                lturns[flight]["P"]["ETD"] = lturns[flight]["D"]["ETA"]
-        return lturns
+                lturns["SPLIT"][str(flight)+"A"]["ETD"] = self.__schedule[flight]["ETA"] + timedelta(minutes=45)
+                lturns["SPLIT"][str(flight)+"D"]["ETA"] = self.__schedule[flight]["ETD"] - timedelta(minutes=45)
+                lturns["SPLIT"][str(flight)+"P"]["ETA"] = lturns["SPLIT"][str(flight)+"A"]["ETD"]
+                lturns["SPLIT"][str(flight)+"P"]["ETD"] = lturns["SPLIT"][str(flight)+"D"]["ETA"]
+
+                if isinstance(self.__schedule[flight]["PREF"][0], list):
+                    lturns["FULL"][flight]["PREF"] = self.__schedule[flight]["PREF"][0]
+                    lturns["SPLIT"][str(flight)+"A"]["PREF"] = self.__schedule[flight]["PREF"][0]
+                    lturns["SPLIT"][str(flight)+"D"]["PREF"] = self.__schedule[flight]["PREF"][1]
+                del lturns["SPLIT"][str(flight)+"P"]["PREF"]
+                del turns[flight]
+        return turns, lturns
+
+    def return_turns(self):
+        return self.__turns
+
+    def return_lturns(self):
+        return self.__lturns
+
+    def return_ac(self):
+        return self.__ac
 
 
 if __name__ == "__main__":
-    s = scheduler()
+    schedule = Scheduler()
